675809e390b0fd56b83993f00f53b752c3364dae
Vertex765 <vertexfinanceiraltda@gmail.com>
2025-08-22 20:36:27 -0300

    TESTIMGS

--- web_vertex/settings.py
+++ web_vertex/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # <---
 ]
 
 ROOT_URLCONF = 'web_vertex.urls'
@@ -132,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # para
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
